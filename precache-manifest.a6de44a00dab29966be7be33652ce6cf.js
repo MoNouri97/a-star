4700cc1d20791e3ee8ef30f03f516596bb972f97
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-07-07 20:53:43 +0100

    Updates

---
+++ precache-manifest.a6de44a00dab29966be7be33652ce6cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eeeedff7754794b6a06d779076fe1e",
+    "url": "/a-star/index.html"
+  },
+  {
+    "revision": "1462804fd98e79607560",
+    "url": "/a-star/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "8c15ae49e50500b1da1f",
+    "url": "/a-star/static/css/main.3e25dc2f.chunk.css"
+  },
+  {
+    "revision": "1462804fd98e79607560",
+    "url": "/a-star/static/js/2.8e1d482a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a-star/static/js/2.8e1d482a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c15ae49e50500b1da1f",
+    "url": "/a-star/static/js/main.2dd10628.chunk.js"
+  },
+  {
+    "revision": "bc73fc556b1b58dc190b",
+    "url": "/a-star/static/js/runtime-main.4f94c0cb.js"
+  }
+]);
