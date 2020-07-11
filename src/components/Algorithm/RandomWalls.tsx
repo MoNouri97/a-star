5d810452563c213b18d3f2ca69c23f75e16f5dad
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-07-11 23:48:14 +0100

    random wall generator

---
+++ src/components/Algorithm/RandomWalls.tsx
@@ -0,0 +1,24 @@
+export const randomWalls: (
+	cols: number,
+	rows: number,
+) => {
+	wall: boolean;
+	delay: number;
+}[][] = (cols: number, rows: number) => {
+	const grid = [];
+	for (let row = 0; row < rows; row++) {
+		const currentRow = [];
+		for (let col = 0; col < cols; col++) {
+			currentRow.push({
+				wall: false,
+				delay: 0,
+			});
+			if (Math.random() < 0.2) {
+				currentRow[col].delay = col * 100;
+				currentRow[col].wall = true;
+			}
+		}
+		grid.push(currentRow);
+	}
+	return grid;
+};
